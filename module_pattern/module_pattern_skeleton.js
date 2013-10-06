60e4e10a4ede2102277226fdfd0e0272b1f6e2e3
Mark Muskardin <mark.muskardin@gmail.com>
2013-10-06 12:14:44 -0700

    module pattern skeletons added

---
+++ module_pattern/module_pattern_skeleton.js
@@ -0,0 +1,21 @@
+MYAPP = MYAPP || {}; // namespace
+
+MYAPP.moduleName.array = (function (){ // private scope provided by immediate function
+	
+	// declare private variables here
+	
+	// declare private methods heres
+	
+	// optionally one-time init procedures here
+
+	return {
+		inArray: function (needle, haystack) {
+			// method code here
+		},
+		isArray: function (a) {
+			// method code here
+		}
+	};
+
+}());
+
