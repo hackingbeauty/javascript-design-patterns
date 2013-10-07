91ffc3759c4bbc8aa2e8df23f93408d3e4870d16
Mark Muskardin <mark.muskardin@gmail.com>
2013-10-06 17:27:05 -0700

    added decorator pattern that does not use inheritance - way simple

--- design_patterns/decorator_pattern/decorator_pattern_without_inheritance.js
+++ design_patterns/decorator_pattern/decorator_pattern_without_inheritance.js
@@ -1,11 +1,11 @@
 function Sale(price) {
-	this.price = (price > 0) || 100;
+	this.price = price || 100;
 	this.decorators_list = [];
 }
 
 Sale.decorators = {};
 
-Sale.decorators.fedTax = {
+Sale.decorators.fedtax = {
 	getPrice: function(price) {
 		return price + price * 5 / 100;
 	}
@@ -22,3 +22,25 @@ Sale.decorators.money = {
 		return "$" + price.toFixed(2);
 	}
 };
+
+Sale.prototype.decorate = function(decorator) {
+	this.decorators_list.push(decorator);
+}
+
+Sale.prototype.getPrice = function(){
+	var price = this.price,
+		name;
+
+	for(var i = 0; i< this.decorators_list.length; i+=1){
+		name = this.decorators_list[i];
+		price = Sale.decorators[name].getPrice(price);
+		console.log("current price is: ", price);
+	}
+	return price;
+}
+
+var sale = new Sale(100);
+sale.decorate('fedtax');
+sale.decorate('quebec');
+sale.decorate('money');
+console.log('the price is: ', sale.getPrice());
